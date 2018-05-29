d5d1dae113094ea6972f5175ce5b8d3bd0e33ade
piosif <patriciaiosif@yahoo.com>
2018-05-29 17:48:24 +0300

    Added mytwitter api

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup
 
 setup(
+    pbr=True,
     name="mytwitter",
-    version="0.0.1",
+    version = "0.0.1",
     author="Lucian Petrut",
     description="""
 Sample app showing a few concepts such as rpc via amqp,
@@ -14,6 +15,7 @@ sqlalchemy, logging, simple config parsing, argument parsing.
         'console_scripts': [
             'mytwitter_client = mytwitter.cmd.client:main',
             'mytwitter_server = mytwitter.cmd.server:main',
+            'mytwitter_api = mytwitter.cmd.api:main',
         ],
     }
 )
